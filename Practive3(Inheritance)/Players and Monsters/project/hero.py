d3db9c6e9c939d42ed90eb5324366c5475d7b7f3
JonDoe841 <mordoz1.0@gmail.com>
2025-02-27 19:01:07 +0200

    Lecture

--- Practive3(Inheritance)/Players and Monsters/project/hero.py
+++ Practive3(Inheritance)/Players and Monsters/project/hero.py
@@ -3,4 +3,4 @@ class Hero:
         self.username = username
         self.level = level
     def __str__(self):
-        return f"{self.name} of type {self.class_name} has level {self.level}"
+        return f"{self.username} of type {self.__class__.__name__} has level {self.level}"
